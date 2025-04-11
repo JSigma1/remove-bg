5493e8473b9add6d6cadd15f80247a10b607c2f0
Jigsaw <168390327+JSigma1@users.noreply.github.com>
2025-04-11 11:37:59 +0700

    Update remove_bg.py

--- remove_bg.py
+++ remove_bg.py
@@ -1,10 +1,10 @@
 from rembg import remove
 from PIL import Image
 
-with open('glibi.jpg','rb') as img_file:
+with open('glibi.jpg','rb') as img_file: #glibi.jpg is your picture file
     input_image = img_file.read()
 
 output_image = remove(input_image)
 
-with open('output.png','wb') as output_file:
+with open('output.png','wb') as output_file: #output.png is your name of file when complete
     output_file.write(output_image)
